97ee813d7c38e34c14209c5fb928242326fcdc44
Jake Benilov <benilov@gmail.com>
2016-11-07 20:47:12 -0800

    ULNs generated by this app shouldn't be valid Luhn numbers
    so we don't inadvertently generate real ones.

--- app/models/uln.rb
+++ app/models/uln.rb
@@ -1,3 +1,14 @@
+require 'luhn'
+
 class Uln < ApplicationRecord
   validates_uniqueness_of :number
+
+  validate :number_is_not_a_valid_luhn_number
+
+private
+  def number_is_not_a_valid_luhn_number
+    if Luhn.valid?(number.to_i)
+      errors.add(:number, "cannot be a Luhn-validated number")
+    end
+  end
 end
